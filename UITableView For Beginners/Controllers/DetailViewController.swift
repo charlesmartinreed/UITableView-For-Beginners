8f5a4d7c08a1f4b19768a54eb5c8fe3b8e13e8e3
Charles Martin Reed <charlesmartinreed@icloud.com>
2019-01-15 19:18:47 -0600

    Add navbar titleView containing Youtube Logo to detail view controller.

--- UITableView For Beginners/Controllers/DetailViewController.swift
+++ UITableView For Beginners/Controllers/DetailViewController.swift
@@ -31,6 +31,26 @@ class DetailViewController: UIViewController {
             videoDescriptionLabel.text = video.description
             videoURL = URL(string: video.url)
         }
+        
+        addNavBarImage()
+    }
+    
+    func addNavBarImage() {
+        let navController = navigationController!
+        
+        let image = #imageLiteral(resourceName: "youtube-logo")
+        let imageView = UIImageView(image: image)
+        
+        let bannerWidth = navController.navigationBar.frame.size.width
+        let bannerHeight = navController.navigationBar.frame.size.height
+        
+        let bannerX = bannerWidth / 2 - image.size.width / 2
+        let bannerY = bannerHeight / 2 - image.size.height / 2
+        
+        imageView.frame = CGRect(x: bannerX, y: bannerY, width: bannerWidth, height: bannerHeight)
+        imageView.contentMode = .scaleAspectFit
+        
+        navigationItem.titleView = imageView
     }
     
     //MARK:- IBActions
